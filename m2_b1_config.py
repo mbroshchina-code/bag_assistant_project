99a1a6995d9eb72dbb3396daf6341ea8d1538c0d
mbroshchina-code <mbroshchina@gmail.com>
2026-06-06 16:21:30 +0300

    Update m2_b1_config.py
    Signed-off-by: mbroshchina-code <mbroshchina@gmail.com>

--- m2_b1_config.py
+++ m2_b1_config.py
@@ -1,10 +1,17 @@
 PROVIDERS = {
+    #"1": {
+    #    "name": "OpenRouter",
+    #    "api_key": None,
+    #    "base_url": "https://openrouter.ai/api/v1",
+    #    "model": "gpt-oss-20b:free",
+    #    "env_key": "OPENROUTER_API_KEY",
+    #},
     "1": {
         "name": "OpenAI",
         "api_key": None,
-        "base_url": "https://openrouter.ai/api/v1",
-        "model": "gpt-oss-20b:free",
-        "env_key": "OPENROUTER_API_KEY",
+        "base_url": "https://api.openai.com/v1",
+        "model": "gpt-4o-mini",
+        "env_key": "OPENAI_API_KEY",
     },
     "2": {
         "name": "Google",
